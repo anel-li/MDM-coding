73a0c9ec2ecf7e1e55997fb5738696f1ad2179d5
el.le <e.linek@web.de>
2021-10-16 21:03:43 +0200

    Add files via upload

---
+++ data vis/Lab Exercise Datasaurus.R
@@ -0,0 +1,32 @@
+#######################################################################################
+# Lab Exercise: Datasaurus
+######################################################################################
+
+library(foreign)
+data <- read.csv("YOURPATH/datasaurus.csv", sep=";")
+
+# This dataset is really a collection of 13 different datasets 
+# (as indicated by the variable "dataset").
+#
+# 1. Choose 4 datasets to work with.
+# 2. Explore those 4 datasets using only descriptive statistics (DO NOT PLOT THEM YET!)
+#    Summarize your findings.
+# 3. Correlate the x and y variables and run simple regressions for each dataset.
+#    Summarize your findings.
+# 4. Now plot the variables for each dataset.
+#    Summarize your findings.
+# 5. If you have not chosen it yet, look at dataset 4.
+#    Summarize your findings.
+#
+#  If you are new to R, have a look at the following two videos
+#  on the course site: 
+#  "suv752-code-01-How-to-do-it-in-R"
+#  "suv752-code-02-Creating-Plots-Using-High-Level-Functions"
+
+# You may also find the following functions helpful:
+#
+# mean()
+# sd()
+# cor()
+# lm()
+######################################################################################
